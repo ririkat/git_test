ffec93216880f88f312dc8a781df7a6451ebe1e6
SEONDEOK <tjtjsejr204@naver.com>
2019-09-04 12:06:42 +0900

    펜션 dao,service,property 수정

--- src/com/jb/pension/model/dao/PensionDao.java
+++ src/com/jb/pension/model/dao/PensionDao.java
@@ -1,5 +1,22 @@
 package com.jb.pension.model.dao;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 public class PensionDao {
+	
+	private Properties prop = new Properties();
+	
+	public PensionDao() {
+		String path = PensionDao.class.getResource("/sql/member/pension-query.properties").getPath();
+		try {
+			prop.load(new FileReader(path));
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
 
 }
--- src/com/jb/pension/model/service/PensionService.java
+++ src/com/jb/pension/model/service/PensionService.java
@@ -1,5 +1,9 @@
 package com.jb.pension.model.service;
 
+import com.jb.pension.model.dao.PensionDao;
+
 public class PensionService {
+	
+	private PensionDao dao = new PensionDao();
 
 }
