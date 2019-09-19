eabc07e80d1d2bb83487deffca197fe663d1360d
SEONDEOK <tjtjsejr204@naver.com>
2019-09-19 18:11:16 +0900

    관리자 승인 대기중인 업주 승인/삭제 구현. 관리자 메뉴 및 검색 디테일 수정.

---
+++ src/com/jb/master/controller/MasterWaitOwnerDeleteServlet.java
@@ -0,0 +1,62 @@
+package com.jb.master.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.jb.client.model.vo.Client;
+import com.jb.owner.model.service.OwnerService;
+
+/**
+ * Servlet implementation class MasterWaitOwnerDeleteServlet
+ */
+@WebServlet("/master/waitOwnerDelete")
+public class MasterWaitOwnerDeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MasterWaitOwnerDeleteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Client loginClient = (Client) request.getSession().getAttribute("loginClient");
+		if (loginClient==null || loginClient.getAuthority()!=1) {
+			request.setAttribute("msg", "잘못된 경로로 접근하셨습니다.");
+			request.setAttribute("loc", "/");
+			request.getRequestDispatcher("/views/common/msg.jsp").forward(request, response);
+			return;
+		}
+		
+		//삭제할 업주 아이디 받아오기
+		String delList = request.getParameter("delOwnerList");
+		System.out.println(delList);
+		
+		int result = new OwnerService().deleteOwnerList(delList);
+		
+		String msg = result>0?"업주 삭제 완료":"업주 삭제 실패";
+		String loc = "/master/waitOwnerList";
+		request.setAttribute("msg", msg);
+		request.setAttribute("loc", loc);
+		request.getRequestDispatcher("/views/common/msg.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
